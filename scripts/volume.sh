096d378b25f3aa652f0a98e7497df23630c7c250
ArcWandx86 <arcwandx86@gmail.com>
2023-04-11 12:23:18 -0400

    Make volume always round to a multiple of 5

---
+++ scripts/volume.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+vfl=$(amixer get Master | grep "Front Left:" | awk -F'[][ %]' '{printf $8}')
+vfr=$(amixer get Master | grep "Front Right:" | awk -F'[][ %]' '{printf $8}')
+
+v=$((($vfl+$vfr)/2))
+v=$(($v/5*5))
+
+amixer set Master unmute
+amixer set Master $v%
+amixer set Master $1
+
+
