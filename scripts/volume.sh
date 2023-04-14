977886f2a83faee29942a0852e0e92cb1afc9fb1
ArcWandx86 <arcwandx86@gmail.com>
2023-04-14 10:30:37 -0400

    Use only one amixer call

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -3,10 +3,8 @@ vfl=$(amixer get Master | grep "Front Left:" | awk -F'[][ %]' '{printf $8}')
 vfr=$(amixer get Master | grep "Front Right:" | awk -F'[][ %]' '{printf $8}')
 
 v=$((($vfl+$vfr)/2))
-v=$(($v/5*5))
+v=$(($v/5*5 + $1))
 
 amixer set Master unmute
 amixer set Master $v%
-amixer set Master $1
-
 
