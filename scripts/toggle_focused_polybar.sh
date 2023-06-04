69bd8fb9426d94a646822d3f8e660a131348ad07
ArcWandx86 <arcwandx86@gmail.com>
2023-06-04 15:15:52 -0400

    Change toggling polybar visibility

--- scripts/toggle_focused_polybar.sh
+++ scripts/toggle_focused_polybar.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/zsh
 
-local monitors=(`polybar --list-monitors | cut -d":" -f1`)
-local pids=(`pidof polybar`)
-
-local focused=`bspc query -M -m focused --names`
-local ind=0
-for i in {1..${#monitors}}; do
-	if [[ "${monitors[i]}" == "${focused}" ]]; then
-		ind=i;
-		break
-	fi
-done
-
-local poly_id="${pids[((${#monitors} + 1 - $ind))]}"
-polybar-msg -p "$poly_id" cmd toggle
+polybar-msg cmd toggle && bspc config top_padding 0
 
