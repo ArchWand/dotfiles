94b65527801eb8726facf1d2e59d50cac7ddb92f
Robert Zhu <arcwandx86@gmail.com>
2024-08-18 17:42:46 -0400

    Change brightness in increments of 5

--- desktop_environment/sxhkd/brightness.sh
+++ desktop_environment/sxhkd/brightness.sh
@@ -4,9 +4,23 @@
 PRIORITY=normal
 # How long the notifications shows for in milliseconds
 TIMEOUT=1000
+# What percentage the brightness should change by
+PERCENTAGE=5
+# How smoothly the brightness should transition
+STEPS=1000
+
+BRIGHTNESS=$(xbacklight -get)
+case $1 in
+	inc)
+		BRIGHTNESS=$((BRIGHTNESS+PERCENTAGE))
+		;;
+	dec)
+		BRIGHTNESS=$((BRIGHTNESS-PERCENTAGE))
+		;;
+esac
 
 # Adjust the brightness
-xbacklight -$1 5 || exit
+xbacklight -set $BRIGHTNESS || exit
 BRIGHTNESS=$(xbacklight -get)
 
 # Send the notification, storing the ID
@@ -14,4 +28,3 @@ notify-send -a "brightness" \
 	-u $PRIORITY -t $TIMEOUT -i display-brightness-symbolic \
 	-h string:wired-tag:system-notif -h int:value:"${BRIGHTNESS}" \
 	"Brightness: ${BRIGHTNESS}%"
-
