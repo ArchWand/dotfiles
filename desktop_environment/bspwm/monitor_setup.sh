9c7d31bd6632be65899d60711cdeb58039c1ccad
Robert Zhu <arcwandx86@gmail.com>
2024-08-18 17:44:23 -0400

    New laptop with new monitor name

--- desktop_environment/bspwm/monitor_setup.sh
+++ desktop_environment/bspwm/monitor_setup.sh
@@ -1,3 +1,7 @@
+# Monitor names
+LAPTOP="eDP"
+EXTERNAL="DisplayPort-3"
+
 # Always use detected autorandr config
 autorandr --change
 ar_conf=$(autorandr | grep detected | cut -d' ' -f 1 | head -n 1)
@@ -8,23 +12,23 @@ fi
 ~/.config/bspwm/desktops.sh
 case $ar_conf in
 	laptop) # Name of the one-screen config
-		start_desk="eDP_$(bspc query -D -d focused --names | cut -d'_' -f 2)"
+		start_desk="$LAPTOP_$(bspc query -D -d focused --names | cut -d'_' -f 2)"
 
 		# Move all nodes to the primary monitor
 		for d in {01..10}; do
 			# Focus next desktop
-			bspc desktop HDMI-1_$d -f &&\
+			bspc desktop $EXTERNAL_$d -f &&\
 			# Select all nodes
 			bspc node -f @/ &&\
 			# Move the nodes
-			bspc node -d eDP-1_$d --follow &&\
+			bspc node -d $LAPTOP_$d --follow &&\
 			# Unselect the nodes
 			bspc node -f @first
 		done
 		~/.config/bspwm/desktops.sh
 
 		# Finally, delete the unused monitor
-		while bspc monitor -f "HDMI-1"; do
+		while bspc monitor -f "$EXTERNAL"; do
 			bspc monitor -r
 		done
 		# And finish on the same desktop
@@ -35,16 +39,16 @@ case $ar_conf in
 		start_desk=$(bspc query -D -d focused --names)
 
 		# Add new monitor if it didn't already exist
-		bspc monitor -f "HDMI-1" || bspc wm --add-monitor "HDMI-1" 2560x1080+1920+0
+		bspc monitor -f "$EXTERNAL" || bspc wm --add-monitor "$EXTERNAL" 2560x1080+1920+0
 
 		# Move all nodes to external monitor
 		# but only if there are no nodes already on the external monitor
-		if [[ -z "$(bspc query -N -m "HDMI-1")" ]]; then
+		if [[ -z "$(bspc query -N -m "$EXTERNAL")" ]]; then
 			# Update the start desktops
-			start_desk="HDMI-1_$(echo "$start_desk" | cut -d'_' -f 2)"
+			start_desk="$EXTERNAL_$(echo "$start_desk" | cut -d'_' -f 2)"
 			# Swap all desktops to the external monitor
 			for d in {01..10}; do
-				bspc desktop eDP-1_$d -s HDMI-1_$d
+				bspc desktop $LAPTOP_$d -s $EXTERNAL_$d
 			done
 		fi
 		# Reset the desktop names
@@ -55,23 +59,23 @@ case $ar_conf in
 		;;
 	monitor) # Name of the external monitor only config
 		# Remember which desktop we started on
-		start_desk="HDMI-1_$(bspc query -D -d focused --names | cut -d'_' -f 2)"
+		start_desk="$EXTERNAL_$(bspc query -D -d focused --names | cut -d'_' -f 2)"
 
 		# Move all nodes to the external monitor
 		for d in {01..10}; do
 			# Focus next desktop
-			bspc desktop eDP-1_$d -f &&\
+			bspc desktop $LAPTOP_$d -f &&\
 			# Select all nodes
 			bspc node -f @/ &&\
 			# Move the nodes
-			bspc node -d HDMI-1_$d --follow &&\
+			bspc node -d $EXTERNAL_$d --follow &&\
 			# Unselect the nodes
 			bspc node -f @first
 		done
 		~/.config/bspwm/desktops.sh
 
 		# Finally, delete the unused monitor
-		while bspc monitor -f "eDP-1"; do
+		while bspc monitor -f "$LAPTOP"; do
 			bspc monitor -r
 		done
 		# And finish on the same desktop
