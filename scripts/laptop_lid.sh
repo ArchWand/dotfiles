0179c1436f29307b2d68781716c43a254f210f6f
Robert Gu <arcwandx86@gmail.com>
2025-04-08 11:57:18 -0400

    Be more user friendly

--- scripts/laptop_lid.sh
+++ scripts/laptop_lid.sh
@@ -2,23 +2,24 @@
 
 file="/etc/systemd/logind.conf"
 param="HandleLidSwitch"
+valid_values=(suspend ignore)
 
 # if no parameters given, print current value
 if [ -z "$1" ]; then
 	value=$(grep -E "^\s*${param}\s*=" "$file" | sed "s/.*=\s*//")
 	echo "Current value: ${value}"
+	echo "Valid values: ${valid_values[@]}"
 	exit 0
 fi
 
 value=$1
-valid_values=(suspend ignore)
 if [[ ! " ${valid_values[@]} " =~ " ${value} " ]]; then
 	echo "Invalid value: ${value}"
 	echo "Valid values: ${valid_values[@]}"
 	exit 1
 fi
 
-sudo -v
+sudo -v || exit
 
 # Edit $param=$value
 sudo sed -i -e 's/^#\?\('"$param"'=\)\(.*\)\(#.*\)\?$/\1'"$value"'/' "$file"
