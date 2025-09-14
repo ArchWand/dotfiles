3c12824f85668d8ad148944f228bed1ea5a574e2
Robert Gu <arcwandx86@gmail.com>
2025-09-14 15:53:19 -0500

    Add yazi

---
+++ tools/yazi/init.lua
@@ -0,0 +1,18 @@
+require("git"):setup()
+
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = "vim", mode="dir" },
+	persist = "none",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	custom_desc_input = false,
+	notify = {
+		enable = false,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
